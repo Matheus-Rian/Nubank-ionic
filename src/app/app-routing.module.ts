97a026822a974678bc5455ca73465427ab0abad9
Matheus Rian <souzamatheusrian@gmail.com>
2020-12-21 17:47:24 -0200

    Clone interface nubank com ionic 5 finalizado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
